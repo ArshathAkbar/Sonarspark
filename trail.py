3d617d0b252d655afe68c0a2a10da00bfeb9350b
ArshathAkbar <140537826+ArshathAkbar@users.noreply.github.com>
2024-02-27 09:40:44 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -1,12 +1,22 @@
-from pyspark.sql import SparkSession
-# Create a SparkSession
-spark = SparkSession.builder \
-    .appName("DiabetesAnalysis") \
-    .getOrCreate()
-# Read CSV file into a DataFrame
-url = 'https://raw.githubusercontent.com/ArshathAkbar/Sonarspark/main/diabetes1.csv'
-df = spark.read.csv(url, header=True, inferSchema=True)
-# Perform some basic operations
-df.show()
-# Stop the SparkSession
-spark.stop()
+class Calculator:
+    def __init__(self):
+        pass
+    def add(x, y):
+        return x + y
+    def subtract(x, y):
+        return x - y
+    def multiply(x, y):
+        return x * y
+    def divide(x, y):
+        if y == 0:
+            raise ValueError("Division by zero is not allowed.")
+        return x / y
+calculator = Calculator()
+result_add = calculator.add(10, 5)
+result_subtract = calculator.subtract(10, 5)
+result_multiply = calculator.multiply(10, 5)
+result_divide = calculator.divide(10, 5)
+print("Addition:", result_add)
+print("Subtraction:", result_subtract)
+print("Multiplication:", result_multiply)
+print("Division:", result_divide)
